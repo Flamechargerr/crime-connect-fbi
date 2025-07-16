0f09b1e27019bb780ca5c7a1c95d5751701414ca
Anamay Tripathy <101051135+Flamechargerr@users.noreply.github.com>
2025-07-16 22:59:40 +0530

    Delete src directory

--- src/components/corkboard/NoteIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { StickyNote } from 'lucide-react';
-
-const NoteIcon: React.FC = () => {
-  return <StickyNote className="h-6 w-6" />;
-};
-
-export default NoteIcon;
