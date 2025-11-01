28751175e1aab34f345bccd901636d802ec44ea2
Flamechargerr <anamaytripathy348@gmail.com>
2025-11-01 13:28:20 +0530

    Update FBI alert dialog component comment

--- frontend/src/components/ui/alert-dialog.tsx
+++ frontend/src/components/ui/alert-dialog.tsx
@@ -4,7 +4,7 @@ import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
-// FBI AlertDialog Component
+// FBI AlertDialog Component - Security Protocol
 const AlertDialog = AlertDialogPrimitive.Root
 
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger
