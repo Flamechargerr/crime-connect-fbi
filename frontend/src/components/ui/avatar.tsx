3fb2eda8c352d5cb5de80dd02c72c88f1d018e8f
Flamechargerr <anamaytripathy348@gmail.com>
2025-11-01 13:28:38 +0530

    Update FBI avatar component comment

--- frontend/src/components/ui/avatar.tsx
+++ frontend/src/components/ui/avatar.tsx
@@ -3,7 +3,7 @@ import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
 import { cn } from "@/lib/utils"
 
-// FBI Avatar Component
+// FBI Avatar Component - Classified
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
