cc5b5f5666e5cdd38d96630b0598644fdcd0adc2
Flamechargerr <anamaytripathy348@gmail.com>
2025-11-01 13:27:07 +0530

    Update FBI card component comment

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-// FBI Card Component
+// FBI Card Component - Classified
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
