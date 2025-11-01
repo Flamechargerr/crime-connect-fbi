09386c00ddf3906a99c37c45ad5ce1b0ee22df20
Flamechargerr <anamaytripathy348@gmail.com>
2025-11-01 13:33:06 +0530

    Update FBI aspect ratio component comment

--- frontend/src/components/ui/aspect-ratio.tsx
+++ frontend/src/components/ui/aspect-ratio.tsx
@@ -1,6 +1,6 @@
 import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
 
-// FBI AspectRatio Component
+// FBI AspectRatio Component - Classified
 const AspectRatio = AspectRatioPrimitive.Root
 
 export { AspectRatio }
